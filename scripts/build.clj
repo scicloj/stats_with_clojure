64daa63d70276aedfdbdad20c52a434697a8279b
Karthikeyan A K <mindaslab@protonmail.com>
2026-02-24 22:57:09 +0530

    Left side TOC with chapter search

--- scripts/build.clj
+++ scripts/build.clj
@@ -2,7 +2,8 @@
   (:require [scicloj.clay.v2.api :as clay]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.java.shell :as sh]))
 
 (defn build-all []
   (let [config (edn/read-string (slurp "clay.edn"))
@@ -14,8 +15,20 @@
                    (map #(.getPath %)))]
     (doseq [f files]
       (println "Building" f "...")
+      ;; We use Clay to generate the .qmd files.
+      ;; Clay currently renders them individually, but we'll re-render the whole site
+      ;; at the end to ensure the searchable index and sidebar are correctly built.
       (clay/make! (merge config {:source-path f
-                                 :browse false})))))
+                                 :browse false})))
+
+    (println "Finalizing searchable index and sidebar...")
+    (let [{:keys [exit _out err]} (sh/with-sh-dir "docs" (sh/sh "quarto" "render" "."))]
+      (if (zero? exit)
+        (println "Documentation built successfully with searchable index.")
+        (do
+          (println "Error during final Quarto render:")
+          (println err)
+          (System/exit exit))))))
 
 (defn -main [& _args]
   (build-all)
